f6b536cc61de3fe36fd8fc0dd44756172d1bc5cc
Vadym <vadymkolomi@gmail.com>
2025-03-07 20:02:17 +0200

    add variables for deploy

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qwe20236@localhost/hotel_booking"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
